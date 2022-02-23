45542f9e23ed5cac9c82f19b95bf39506e82d56b
wagnerkaba <bom.link+github@gmail.com>
2022-02-23 14:02:45 -0300

    correção bug

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', request, response) => {
-    return response.send(200).json({message: 'servidor ok'});
-}
+app.get('/', (request, response) => {
+    return response.json({message: 'servidor ok'});
+});
 
 app.listen(3333);
