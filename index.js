86edb83b1c0758313a7210b2fa048ce6931ac287
wagnerkaba <bom.link+github@gmail.com>
2022-02-23 16:40:57 -0300

    teste com formulário

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const { response } = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (request, response) => {
-    return response.json({message: 'servidor ok'});
+app.post('/', (request, response) => {
+
+    const resposta = request.body;
+    console.log(resposta);
+    return response.send(`Olá ${resposta.nome}! Seja bem vindo! ${resposta.texto}`);
 });
 
 app.listen(3333);
